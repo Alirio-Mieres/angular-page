a2057c7eb10a1456f72e1e98dd68af821284b0f4
alirio <mie21001@byui.edu>
2022-11-14 15:45:21 -0400

    fix error

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -61,7 +61,7 @@ export class ProductosService {
       
       const tituloLower = prod.titulo.toLocaleLowerCase();
 
-      if( prod.categoria.indexOf( termino ) >=0 && tituloLower.indexOf( termino ) >=0) {
+      if( prod.categoria.indexOf( termino ) >=0 || tituloLower.indexOf( termino ) >=0) {
         this.productosFiltrado.push( prod );
       }
 
